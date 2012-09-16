765f8a2713caf29eba9ad5bcc371fa5de0f84d84
Sparanoid <Sparanoid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-16 15:27:51 +0000

    Adding first version of my plugin
    git-svn-id: http://plugins.svn.wordpress.org/relative-url/trunk@599673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Relative URL ===
+Contributors: Sparanoid
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=VJRCEYRS8GTYE&lc=C2&item_name=Plugin%20Donation&button_subtype=services&currency_code=USD&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted
+Tags: admin, administration, comment, comments, content, contents, excerpt, excerpts, feed, feeds, html, multisite, page, pages, plugin, plugins, post, posts, template, templates, text, title, wp_make_link_relative, widget, widgets, wpmu, writing
+Requires at least: 2.1.0
+Tested up to: 3.4
+Stable tag: 2.6.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin applies wp_make_link_relative function to links (posts, categories, pages and etc.) to convert them to relative URLs.
+
+== Description ==
+A plugin applies wp_make_link_relative function to links (posts, categories, pages and etc.) to convert them to relative URLs.
+
+== Installation ==
+
+WordPress (Multisite ready):
+
+1. Upload the extracted files to the `/wp-content/plugins/` directory
+2. In 'Plugins' page, choose 'Activate'
+
+== Upgrade Notice ==
+
+= 0.0.1 =
+* First release
+
+== Changelog ==
+
+= 0.0.1 =
+* First release
---
+++ relative-url.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Plugin Name: Relative URL
+Plugin URI: http://sparanoid.com/lab/relative-url/
+Description: A plugin applies wp_make_link_relative function to links (posts, categories, pages and etc.) to convert them to relative URLs.
+Version: 0.1
+Author: Tunghsiao Liu
+Author URI: http://sparanoid.com/
+Author Email: info@sparanoid.com
+License: GPLv2 or later
+
+  Copyright 2012 Tunghsiao Liu, aka. Sparanoid (info@sparanoid.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+  // Modified from https://github.com/retlehs/roots/issues/490
+
+  // This makes paths realitive for NEW content.
+  // For previously posted content, full urls are already loaded in to the database and will need to be updated.
+
+  // for media and image paths - update wp admin: settings>media
+  // set "Full URL path to files" feild to '/your-upload-folder-name/'
+  // This would be '/assets/' for a default roots install.
+
+  // http://www.deluxeblogtips.com/2012/06/relative-urls.html
+
+  add_action( 'template_redirect', 'rw_relative_urls' );
+
+  function rw_relative_urls() {
+    // Don't do anything if:
+    // - In feed
+    // - In sitemap by WordPress SEO plugin
+    if ( is_feed() || get_query_var( 'sitemap' ) )
+      return;
+    $filters = array(
+      'post_link',       // Normal post link
+      'post_type_link',  // Custom post type link
+      'page_link',       // Page link
+      'attachment_link', // Attachment link
+      'get_shortlink',   // Shortlink
+      'post_type_archive_link',    // Post type archive link
+      'get_pagenum_link',          // Paginated link
+      'get_comments_pagenum_link', // Paginated comment link
+      'term_link',   // Term link, including category, tag
+      'search_link', // Search link
+      'day_link',   // Date archive link
+      'month_link',
+      'year_link',
+
+      // site location
+      'option_siteurl',
+      'blog_option_siteurl',
+      'option_home',
+      'admin_url',
+      'home_url',
+      'includes_url',
+      'site_url',
+      'site_option_siteurl',
+      'network_home_url',
+      'network_site_url',
+
+      // debug only filters
+      'get_the_author_url',
+      'get_comment_link',
+      'wp_get_attachment_image_src',
+      'wp_get_attachment_thumb_url',
+      'wp_get_attachment_url',
+      'wp_login_url',
+      'wp_logout_url',
+      'wp_lostpassword_url',
+      'get_stylesheet_uri',
+      'get_stylesheet_directory_uri',
+      // 'plugins_url',
+      // 'plugin_dir_url',
+      'stylesheet_directory_uri',
+      'get_template_directory_uri',
+      'template_directory_uri',
+      'get_locale_stylesheet_uri',
+      'script_loader_src', // plugin scripts url
+      'style_loader_src', // plugin styles url
+      'get_theme_root_uri'
+      // 'home_url'
+    );
+
+    foreach ( $filters as $filter ) {
+      add_filter( $filter, 'wp_make_link_relative' );
+    }
+    home_url($path = '', $scheme = null);
+  }
+?>
