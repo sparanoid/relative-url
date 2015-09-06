af8c2d38eedfdb856563352b9b4b8d96cdb95761
Tunghsiao Liu <t@sparanoid.com>
2015-09-06 16:57:29 +0800

    chore: tabs to spaces, minor update

--- relative-url.php
+++ relative-url.php
@@ -25,89 +25,91 @@ License: GPLv2 or later
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-  // Modified from https://github.com/retlehs/roots/issues/490
+// Modified from https://github.com/retlehs/roots/issues/490
 
-  // This makes paths realitive for NEW content.
-  // For previously posted content, full urls are already loaded in to the database and will need to be updated.
+// This makes paths realitive for NEW content.
+// For previously posted content, full urls are already loaded in to the database and will need to be updated.
 
-  // for media and image paths - update wp admin: settings>media
-  // set "Full URL path to files" feild to '/your-upload-folder-name/'
-  // This would be '/assets/' for a default roots install.
+// for media and image paths - update wp admin: settings>media
+// set "Full URL path to files" feild to '/your-upload-folder-name/'
+// This would be '/assets/' for a default roots install.
 
-  // http://www.deluxeblogtips.com/2012/06/relative-urls.html
+// http://www.deluxeblogtips.com/2012/06/relative-urls.html
 
-  add_action( 'template_redirect', 'relative_url' );
+add_action( 'template_redirect', 'relative_url' );
 
-  function relative_url() {
-    $filters = array(
-      'bloginfo_url',
-      'the_permalink',
-      'wp_list_pages',
-      'wp_list_categories',
-      'the_content_more_link',
-      'the_tags',
-      'the_author_posts_link',
-      'post_link',       // Normal post link
-      'post_type_link',  // Custom post type link
-      'page_link',       // Page link
-      'attachment_link', // Attachment link
-      'get_shortlink',   // Shortlink
-      'post_type_archive_link',    // Post type archive link
-      'get_pagenum_link',          // Paginated link
-      'get_comments_pagenum_link', // Paginated comment link
-      'term_link',   // Term link, including category, tag
-      'search_link', // Search link
-      'day_link',   // Date archive link
-      'month_link',
-      'year_link',
+function relative_url() {
+  $filters = array(
+    'bloginfo_url',
+    'the_permalink',
+    'wp_list_pages',
+    'wp_list_categories',
+    'the_content_more_link',
+    'the_tags',
+    'the_author_posts_link',
+    'post_link',       // Normal post link
+    'post_type_link',  // Custom post type link
+    'page_link',       // Page link
+    'attachment_link', // Attachment link
+    'get_shortlink',   // Shortlink
+    'post_type_archive_link',    // Post type archive link
+    'get_pagenum_link',          // Paginated link
+    'get_comments_pagenum_link', // Paginated comment link
+    'term_link',   // Term link, including category, tag
+    'search_link', // Search link
+    'day_link',   // Date archive link
+    'month_link',
+    'year_link',
 
-      // site location
-      'option_siteurl',
-      'blog_option_siteurl',
-      'option_home',
-      'admin_url',
-      'home_url',
-      'includes_url',
-      'site_url',
-      'site_option_siteurl',
-      'network_home_url',
-      'network_site_url',
+    // site location
+    'option_siteurl',
+    'blog_option_siteurl',
+    'option_home',
+    'admin_url',
+    'get_admin_url',
+    'get_site_url',
+    'network_admin_url',
+    'home_url',
+    'includes_url',
+    'site_url',
+    'site_option_siteurl',
+    'network_home_url',
+    'network_site_url',
 
-      // debug only filters
-      'get_the_author_url',
-      'get_comment_link',
-      'wp_get_attachment_image_src',
-      'wp_get_attachment_thumb_url',
-      'wp_get_attachment_url',
-      'wp_login_url',
-      'wp_logout_url',
-      'wp_lostpassword_url',
-      'get_stylesheet_uri',
-      // 'get_stylesheet_directory_uri',
-      // 'plugins_url',
-      // 'plugin_dir_url',
-      // 'stylesheet_directory_uri',
-      // 'get_template_directory_uri',
-      // 'template_directory_uri',
-      'get_locale_stylesheet_uri',
-      'script_loader_src', // plugin scripts url
-      'style_loader_src', // plugin styles url
-      'get_theme_root_uri'
-      // 'home_url'
-    );
+    // debug only filters
+    'get_the_author_url',
+    'get_comment_link',
+    'wp_get_attachment_image_src',
+    'wp_get_attachment_thumb_url',
+    'wp_get_attachment_url',
+    'wp_login_url',
+    'wp_logout_url',
+    'wp_lostpassword_url',
+    'get_stylesheet_uri',
+    // 'get_stylesheet_directory_uri',
+    // 'plugins_url',
+    // 'plugin_dir_url',
+    // 'stylesheet_directory_uri',
+    // 'get_template_directory_uri',
+    // 'template_directory_uri',
+    'get_locale_stylesheet_uri',
+    'script_loader_src', // plugin scripts url
+    'style_loader_src', // plugin styles url
+    'get_theme_root_uri'
+    // 'home_url'
+  );
 
-    // Thanks to https://wordpress.org/support/topic/request-only-replace-local-urls
-    $home_url = home_url();
-    $filter_fn = function( $link ) use ( $home_url ) {
-      if ( strpos( $link, $home_url ) === 0 ) {
-        return wp_make_link_relative( $link );
-      } else {
-        return $link;
-      }
-    };
-
-    foreach ( $filters as $filter ) {
-      add_filter( $filter, $filter_fn );
+  // Thanks to https://wordpress.org/support/topic/request-only-replace-local-urls
+  $home_url = home_url();
+  $filter_fn = function( $link ) use ( $home_url ) {
+    if ( strpos( $link, $home_url ) === 0 ) {
+      return wp_make_link_relative( $link );
+    } else {
+      return $link;
     }
+  };
+
+  foreach ( $filters as $filter ) {
+    add_filter( $filter, $filter_fn );
   }
-?>
+}
